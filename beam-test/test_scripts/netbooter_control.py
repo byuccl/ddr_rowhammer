91795c00693cb4dd41e0c26f53bdb8bb32c4be38
Mike Wirthlin <wirthlin@byu.edu>
2022-11-11 08:05:29 -0700

    Added telnet error checking and reporting

--- beam-test/test_scripts/netbooter_control.py
+++ beam-test/test_scripts/netbooter_control.py
@@ -51,14 +51,33 @@ class netbooter_control():
         return subprocess.call(command) == 0
 
     def open_telnet(self):
-        #teln = telnetlib.Telnet(self.netbooter_ip_addr, None, timeout=self.TIMEOUT_NETBOOTER)
-        self.teln = telnetlib.Telnet(self.netbooter_ip_addr, timeout=self.TIMEOUT_NETBOOTER)
-        # Does it return an error?
+
+        ''' Open the telnet connection to the netbooter '''
+        try:
+            self.teln = telnetlib.Telnet(self.netbooter_ip_addr, timeout=self.TIMEOUT_NETBOOTER)
+        except (Exception) as error:
+            self._error("Error opening telnet"+str(error))
+            self.teln = None
+            return False
+
         self.telnet_open = True
         return True
 
     def close_telnet(self):
-        self.teln.close()
+        ''' Close telnet connection '''
+        if self.teln:
+            self.teln.close()
+
+    def write_telnet(self, write_str:str):
+        ''' write telnet connection '''
+        if not self.teln:
+            return False
+        try:
+            self.teln.write(write_str)
+        except Exception as error: # EOFException?
+            self._error("Error Writing to telnet "+str(error))
+            return False
+        return True
 
     def _control_string(self,power_port,turn_on):
         ''' Generate a control string for the port/function '''
@@ -71,45 +90,66 @@ class netbooter_control():
 
     def control_port(self, power_port, turn_on = True, cycle=False):
 
-        #print(f"Port {power_port} state {turn_on}")
-        # Open telnet
-        if not self.telnet_open:
-            if not self.open_telnet():
-                return False
-
-        # Read netbooter port to clear buffer
-        s = self.teln.read_some()
-        time.sleep(self.SLEEPTIME_NETBOOTER)
-
         if not turn_on:
-            state = "Off"
-            nState = "On"
+            new_state = "Off"
+            inverted_state = "On"
         else:
-            state = "On"
-            nState = "Off"
+            new_state = "On"
+            inverted_state = "Off"
 
-        if cycle:
-            s = ("pset " + str(power_port) + " 0").encode("ascii") + b"\r\n\r\n"
-            # Do the opposite of what is wanted (for cycle)
-            self.teln.write(self._control_string(power_port, not turn_on))
-            self._info("Setting port "+str(power_port)+" to "+nState)
+        # Attempt command multile times
+        CONTROL_PORT_ATTEMPTS = 5
+        for attempt in range(CONTROL_PORT_ATTEMPTS):
+            self._info(f"Attempt {(attempt+1)} to set port {power_port} with cycle={cycle}")
+
+            # Open telnet
+            if not self.telnet_open:
+                if not self.open_telnet():
+                    # Try again
+                    continue
+            # Opened: wait a bit before communicating
+            time.sleep(1)
+
+            # Read netbooter port to clear buffer
+            try:
+                self.teln.read_some()
+            except (Exception) as error:
+                # try again (close?)
+                #print("Did not read_some")
+                continue
             time.sleep(self.SLEEPTIME_NETBOOTER)
 
+            if cycle:
+                s = ("pset " + str(power_port) + " 0").encode("ascii") + b"\r\n\r\n"
+                # Do the opposite of what is wanted (for cycle)
+                if not self.write_telnet(self._control_string(power_port, not turn_on)):
+                    # Failed write, continue to try again
+                    ##print("Failed write cycle")
+                    continue
+                self._info("Setting port "+str(power_port)+" to "+inverted_state)
+                time.sleep(self.SLEEPTIME_NETBOOTER)
 
-        control_string = self._control_string(power_port, turn_on)
-        self.teln.write(control_string)
-        #print(control_string)
-        self._info(str("Setting port "+str(power_port)+" to "+state))
-        time.sleep(self.SLEEPTIME_NETBOOTER)
-        return True
+            control_string = self._control_string(power_port, turn_on)
+            if not self.write_telnet(control_string):
+                # Failed write, continue to try again
+                #print("Failed write")
+                continue
+            self._info(str("Setting port "+str(power_port)+" to "+new_state))
+            time.sleep(self.SLEEPTIME_NETBOOTER)
+            # This succeeded
+            return True
+
+        # failed all attempts
+        return False
+
 
     def turn_on_port(self, power_port):
-        self.control_port(power_port, True)
-        return True
+        ''' Turn netbooter port on '''
+        return self.control_port(power_port, True)
 
     def turn_off_port(self, power_port):
-        self.control_port(power_port, False)
-        return True
+        ''' Turn netbooter port off '''
+        return self.control_port(power_port, False)
 
     def netbooter_group_args(parser):
         ''' Static function for creating netbooter argument group '''
