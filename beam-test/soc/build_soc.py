023807c045d1564a0ddeb1f370ce1b01d886d4b6
Tommy Cox <git@tommycox.org>
2022-11-14 12:01:49 -0700

    Consolidate SoC modifications to one file.

--- beam-test/soc/build_soc.py
+++ beam-test/soc/build_soc.py
@@ -1,19 +1,40 @@
-# Import modified SoC and rename for compatibility with default build script.
-from beam_test_soc import TestSoC as BaseSoC
-
-# ---------------------------------------------------------------------------- #
-# ---------------------------------------------------------------------------- #
-# ---------------------------------------------------------------------------- #
-# ---------------------------------------------------------------------------- #
-#        Below is copied from litex_boards.targets.digilent_nexys_video        #
-# ---------------------------------------------------------------------------- #
-# ---------------------------------------------------------------------------- #
-# ---------------------------------------------------------------------------- #
-# ---------------------------------------------------------------------------- #
+from litex_boards.targets.digilent_nexys_video import BaseSoC
 
 from litex.build.xilinx.vivado import vivado_build_args, vivado_build_argdict
 from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
+
+class TestSoC(BaseSoC):
+    def __init__(
+        self,
+        toolchain="vivado",
+        sys_clk_freq=...,
+        with_ethernet=False,
+        with_led_chaser=False,  # Changed this default so I can see if the hack worked.
+        with_sata=False,
+        sata_gen="gen2",
+        with_sata_pll_refclk=False,
+        vadj="1.2V",
+        with_video_terminal=False,
+        with_video_framebuffer=False,
+        uart_bone=None,
+        **kwargs
+    ):
+        super().__init__(
+            toolchain,
+            sys_clk_freq,
+            with_ethernet,
+            with_led_chaser,
+            with_sata,
+            sata_gen,
+            with_sata_pll_refclk,
+            vadj,
+            with_video_terminal,
+            with_video_framebuffer,
+            **kwargs
+        )
+        if uart_bone:
+            self.add_uartbone(name=uart_bone, buildrate=115200)
 
 def main():
     from litex.soc.integration.soc import LiteXSoCArgumentParser
@@ -31,6 +52,7 @@ def main():
     target_group.add_argument("--sata-gen",               default="2",         help="SATA Gen.", choices=["1", "2"])
     target_group.add_argument("--with-sata-pll-refclk",   action="store_true", help="Generate SATA RefClk from PLL.")
     target_group.add_argument("--vadj",                   default="1.2V",      help="FMC VADJ value.", choices=["1.2V", "1.8V", "2.5V", "3.3V"])
+    target_group.add_argument("--uart_bone",default="usb_fifo",help="Add uartbone with given serial device.")
     viopts = target_group.add_mutually_exclusive_group()
     viopts.add_argument("--with-video-terminal",    action="store_true", help="Enable Video Terminal (HDMI).")
     viopts.add_argument("--with-video-framebuffer", action="store_true", help="Enable Video Framebuffer (HDMI).")
@@ -39,7 +61,7 @@ def main():
     vivado_build_args(parser)
     args = parser.parse_args()
 
-    soc = BaseSoC(
+    soc = TestSoC(
         toolchain              = args.toolchain,
         sys_clk_freq           = int(float(args.sys_clk_freq)),
         with_ethernet          = args.with_ethernet,
