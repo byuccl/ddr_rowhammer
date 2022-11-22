431316f38e47b552569d6b19b12fb21d699dab5b
Mike Wirthlin <wirthlin@byu.edu>
2022-11-21 17:40:43 -0700

    Debugging

--- beam-test/test_scripts/ddrdata_experiment.py
+++ beam-test/test_scripts/ddrdata_experiment.py
@@ -48,6 +48,8 @@ UART_BASENAME = "uart"
 UART_BAUD_RATE = 115200
 UART_PHYS_PORT = "1-4.1"
 UART_PHYS_IF = 0
+DEFAULT_LITEX_LOGIN_DELAY = 15
+DEFAULT_IDENT_ADDRESS = 0xf0001800
 
 # State constants
 INITIAL_STARTING_STATE = "Initial Starting State"
@@ -57,12 +59,26 @@ UART_SETUP_STATE = "UART Setup State"
 POWER_NEXYS_STATE = "Power Nexys State"
 CONNECT_UART_STATE = "Connect UART State"
 CONFIGURE_NEXYS_STATE = "Configure Nexys State"
-
-#ENABLE_SCRUBBING_STATE = "Enable Scrubbing State"
-#LITEX_PROMPT_STATE = "LiteX Login State"
+ENABLE_SCRUBBING_STATE = "Enable Scrubbing State"
+LITEX_PROMPT_STATE = "LiteX Login State"
 
 
 TERMINATING_STATE = "Terminating State"
+
+def expect_prompt(ex, number_of_enters=1,expect_timeout=DEFAULT_LITEX_LOGIN_DELAY):
+    ''' Send "Enter" and expects the "litex" prompt (does this once)
+        Does NOT set state (state actions should set state)
+        Returns True if expect was successful, False otherwise
+          Calling functions should query for details about failed expect
+    '''
+    # Todo: Allow multiple attempts (if boot is slow)
+    LITEX_LOGIN_PATTERN = "^.*litex[^>]*> "
+    for i in range(number_of_enters):
+        ex.uart.sendline("\n")
+    ex.uart.expect(LITEX_LOGIN_PATTERN,timeout=expect_timeout)
+    if ex.uart.has_error():
+        return False
+    return True
 
 def initial_starting_state_actions(ex):
     ''' Do nothing - just an entry point for the experiment. Executed only once. 
@@ -136,10 +152,53 @@ def connect_uart_state_actions(ex):
 
 def configure_nexys_state_actions(ex):
     if ex.args.disable_jcm:
-        return TERMINATING_STATE
+        return ENABLE_SCRUBBING_STATE
     result = ex.jcm.configure_fpga(ex.args.bitstream)
     if not result:
         return TERMINATING_STATE
+    return ENABLE_SCRUBBING_STATE
+
+def enable_scrubbing_state_actions(ex):
+    if ex.args.disable_jcm:
+        return LITEX_PROMPT_STATE
+
+    if ex.args.disable_scrubbing:
+        ex.scrubbing_ok = True
+        return LITEX_PROMPT_STATE
+
+    ex.scrubbing_ok = False
+    frads_file = None
+    if ex.args.frads_file:
+        frads_file = ex.args.frads_file
+    ITERATIONS = 100_000_000
+
+    if ex.args.fault_injection:
+        inject_faults = ex.args.fault_injection
+    else:
+        inject_faults = 0
+
+    result = ex.jcm.scrub_fpga(iterations=ITERATIONS, frads_file = frads_file, inject_faults = inject_faults, block=False)
+    # TODO: do some real checking on scrubbing
+    return LITEX_PROMPT_STATE
+
+def initial_litex_prompt_state_actions(ex):
+    MAX_LOGIN_ATTEMPS = 3
+    ex.login_litex = False
+    ex.initial_login_terminate = False
+    expect_result = expect_prompt(ex)
+    if expect_result:
+        # All is good - move on
+        return TERMINATING_STATE
+        #ex.login_litex = True
+        #ex.failed_initial_login = 0  # Reset counter for next time around
+    else:
+        # Failed login, go back to repower of NEXYS until max attempts
+        if ex.failed_initial_login >= MAX_LOGIN_ATTEMPS:
+            # Failed too many times - give up
+            ex.initial_login_terminate = True
+        else:
+            # Try again
+            ex.failed_initial_login += 1
     return TERMINATING_STATE
 
 def terminating_state_actions(ex):
@@ -206,6 +265,18 @@ def build_experiment(args,logger,single_step=False):
         configure_nexys_state_actions,
     ))
 
+    # ENABLE_SCRUBBING_STATE
+    experiment.add_state(ExperimentState(
+        ENABLE_SCRUBBING_STATE,
+        enable_scrubbing_state_actions,
+    ))
+
+    # LITEX_PROMPT_STATE
+    experiment.add_state(ExperimentState(
+        LITEX_PROMPT_STATE,
+        initial_litex_prompt_state_actions,
+    ))
+
     # TERMINATING_STATE
     # - Do nothing: place holder for ending state. Will set experiment to "stop"
     # - Enter this state when the experiment cannot continue
@@ -230,7 +301,6 @@ def main():
     parser.add_argument_group(
         usb_uart_base.uart_group_args(parser,UART_BASENAME, default_phys_port = UART_PHYS_PORT, 
         default_phys_if = UART_PHYS_IF, default_baud = UART_BAUD_RATE))
-
 
     parser.add_argument("--log_dir", help="Directory to store log files", type=str)
     parser.add_argument("--single_step", help="Single step through state machine", action='store_true')
