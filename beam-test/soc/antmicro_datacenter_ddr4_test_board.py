f620961d7af99afe422832eabddd51b2fb3054ee
Tyler <tylerbricks@gmail.com>
2022-12-08 13:37:06 -0700

    databoard changes

--- beam-test/soc/antmicro_datacenter_ddr4_test_board.py
+++ beam-test/soc/antmicro_datacenter_ddr4_test_board.py
@@ -11,9 +11,8 @@ import json
 
 from migen import *
 
-from litex.gen import LiteXModule
-
 from litex_boards.platforms import antmicro_datacenter_ddr4_test_board
+from litex.build.xilinx.vivado import vivado_build_args, vivado_build_argdict
 
 from litex.soc.cores.clock import *
 from litex.soc.integration.soc_core import *
@@ -37,23 +36,23 @@ from litespi.opcodes import SpiNorFlashOpCodes as Codes
 
 # CRG ----------------------------------------------------------------------------------------------
 
-class _CRG(LiteXModule):
+class _CRG(Module):
     def __init__(self, platform, sys_clk_freq, iodelay_clk_freq, with_video_pll=False):
-        self.cd_sys       = ClockDomain()
-        self.cd_sys2x     = ClockDomain()
-        self.cd_sys4x     = ClockDomain()
-        self.cd_sys4x_dqs = ClockDomain()
-        self.cd_idelay    = ClockDomain()
+        self.clock_domains.cd_sys       = ClockDomain()
+        self.clock_domains.cd_sys2x     = ClockDomain()
+        self.clock_domains.cd_sys4x     = ClockDomain()
+        self.clock_domains.cd_sys4x_dqs = ClockDomain()
+        self.clock_domains.cd_idelay    = ClockDomain()
 
-        self.cd_hdmi      = ClockDomain()
-        self.cd_hdmi5x    = ClockDomain()
+        self.clock_domains.cd_hdmi      = ClockDomain()
+        self.clock_domains.cd_hdmi5x    = ClockDomain()
 
         # # #
 
         # Clk.
         clk100 = platform.request("clk100")
 
-        self.pll = pll = S7PLL(speedgrade=-1)
+        self.submodules.pll = pll = S7PLL(speedgrade=-1)
         pll.register_clkin(clk100, 100e6)
         pll.create_clkout(self.cd_sys,       sys_clk_freq)
         pll.create_clkout(self.cd_sys2x,     2 * sys_clk_freq)
@@ -61,11 +60,11 @@ class _CRG(LiteXModule):
         pll.create_clkout(self.cd_sys4x_dqs, 4 * sys_clk_freq, phase=90)
         pll.create_clkout(self.cd_idelay,    iodelay_clk_freq)
 
-        self.idelayctrl = S7IDELAYCTRL(self.cd_idelay)
+        self.submodules.idelayctrl = S7IDELAYCTRL(self.cd_idelay)
 
         # Video PLL.
         if with_video_pll:
-            self.video_pll = video_pll = S7MMCM(speedgrade=-1)
+            self.submodules.video_pll = video_pll = S7MMCM(speedgrade=-1)
             video_pll.register_clkin(clk100, 100e6)
             video_pll.create_clkout(self.cd_hdmi,   40e6)
             video_pll.create_clkout(self.cd_hdmi5x, 5*40e6)
@@ -74,38 +73,26 @@ class _CRG(LiteXModule):
 # BaseSoC ------------------------------------------------------------------------------------------
 
 class BaseSoC(SoCCore):
-    def __init__(self, *, sys_clk_freq=100e6, iodelay_clk_freq=200e6,
-            with_ethernet          = False,
-            with_etherbone         = False,
-            eth_ip                 = "192.168.1.50",
-            eth_reset_time         = "10e-3",
-            eth_dynamic_ip         = False,
-            with_hyperram          = False,
-            with_sdcard            = False,
-            with_jtagbone          = True,
-            with_uartbone          = False,
-            with_spi_flash         = False,
-            with_led_chaser        = True,
-            with_video_terminal    = False,
-            with_video_framebuffer = False,
-            **kwargs):
+    def __init__(self, *, sys_clk_freq=int(100e6), iodelay_clk_freq=200e6,
+            with_ethernet=False, with_etherbone=False, eth_ip="192.168.1.50", eth_reset_time="10e-3", eth_dynamic_ip=False,
+            with_hyperram=False, with_sdcard=False, with_jtagbone=True, with_uartbone=False, with_spi_flash=False,
+            with_led_chaser=True, with_video_terminal=False, with_video_framebuffer=False, **kwargs):
         platform = antmicro_datacenter_ddr4_test_board.Platform()
 
         # CRG --------------------------------------------------------------------------------------
         with_video_pll = (with_video_terminal or with_video_framebuffer)
-        self.crg = _CRG(platform, sys_clk_freq, iodelay_clk_freq=iodelay_clk_freq, with_video_pll=with_video_pll)
+        self.submodules.crg = _CRG(platform, sys_clk_freq, iodelay_clk_freq=iodelay_clk_freq, with_video_pll=with_video_pll)
 
         # SoCCore ----------------------------------------------------------------------------------
         SoCCore.__init__(self, platform, sys_clk_freq, ident="LiteX SoC on data center test board", **kwargs)
 
         # DDR4 SDRAM RDIMM -------------------------------------------------------------------------
         if not self.integrated_main_ram_size:
-            self.ddrphy = A7DDRPHY(platform.request("ddr4"),
-                write_latency_calibration = True,
-                memtype                   = "DDR4",
-                iodelay_clk_freq          = iodelay_clk_freq,
-                sys_clk_freq              = sys_clk_freq,
-                is_rdimm                  = True,
+            self.submodules.ddrphy = A7DDRPHY(platform.request("ddr4"),
+                memtype         = "DDR4",
+                iodelay_clk_freq = iodelay_clk_freq,
+                sys_clk_freq     = sys_clk_freq,
+                is_rdimm         = True,
             )
             self.add_sdram("sdram",
                 phy                     = self.ddrphy,
@@ -117,7 +104,7 @@ class BaseSoC(SoCCore):
 
         # HyperRAM ---------------------------------------------------------------------------------
         if with_hyperram:
-            self.hyperram = HyperRAM(platform.request("hyperram"), sys_clk_freq=sys_clk_freq)
+            self.submodules.hyperram = HyperRAM(platform.request("hyperram"), sys_clk_freq=sys_clk_freq)
             self.bus.add_slave("hyperram", slave=self.hyperram.bus, region=SoCRegion(origin=0x20000000, size=8*1024*1024))
 
         # SD Card ----------------------------------------------------------------------------------
@@ -129,7 +116,7 @@ class BaseSoC(SoCCore):
             # Traces between PHY and FPGA introduce ignorable delays of ~0.165ns +/- 0.015ns.
             # PHY chip does not introduce delays on TX (FPGA->PHY), however it includes 1.2ns
             # delay for RX CLK so we only need 0.8ns to match the desired 2ns.
-            self.ethphy = LiteEthS7PHYRGMII(
+            self.submodules.ethphy = LiteEthS7PHYRGMII(
                 clock_pads = self.platform.request("eth_clocks"),
                 pads       = self.platform.request("eth"),
                 rx_delay   = 0.8e-9,
@@ -146,13 +133,13 @@ class BaseSoC(SoCCore):
 
         # Leds -------------------------------------------------------------------------------------
         if with_led_chaser:
-            self.leds = LedChaser(
+            self.submodules.leds = LedChaser(
                 pads         = platform.request_all("user_led"),
                 sys_clk_freq = sys_clk_freq)
 
         # Video ------------------------------------------------------------------------------------
         if with_video_terminal or with_video_framebuffer:
-            self.videophy = VideoS7HDMIPHY(platform.request("hdmi_out"), clock_domain="hdmi")
+            self.submodules.videophy = VideoS7HDMIPHY(platform.request("hdmi_out"), clock_domain="hdmi")
             if with_video_terminal:
                 self.add_video_terminal(phy=self.videophy, timings="800x600@60Hz", clock_domain="hdmi")
             if with_video_framebuffer:
@@ -164,7 +151,7 @@ class BaseSoC(SoCCore):
 
         # System I2C (behing multiplexer) ----------------------------------------------------------
         i2c_pads = platform.request('i2c')
-        self.i2c = I2CMaster(i2c_pads)
+        self.submodules.i2c = I2CMaster(i2c_pads)
 
     def generate_sdram_phy_py_header(self, output_file):
         os.makedirs(os.path.dirname(output_file), exist_ok=True)
@@ -190,36 +177,37 @@ class LiteDRAMSettingsEncoder(json.JSONEncoder):
         return super().default(o)
 
 def main():
-    from litex.build.parser import LiteXArgumentParser
-    parser = LiteXArgumentParser(platform=antmicro_datacenter_ddr4_test_board.Platform, description="LiteX SoC on DDR4 Datacenter Test Board.")
-    parser.add_target_argument("--flash",            action="store_true",       help="Flash bitstream.")
-    parser.add_target_argument("--sys-clk-freq",     default=100e6, type=float, help="System clock frequency.")
-    parser.add_target_argument("--iodelay-clk-freq", default=200e6, type=float, help="IODELAYCTRL frequency.")
-    ethopts = parser.target_group.add_mutually_exclusive_group()
-    ethopts.add_argument("--with-ethernet",                action="store_true",    help="Add Ethernet.")
-    ethopts.add_argument("--with-etherbone",               action="store_true",    help="Add EtherBone.")
-    parser.add_target_argument("--eth-ip",                 default="192.168.1.50", help="Ethernet/Etherbone IP address.")
-    parser.add_target_argument("--eth-dynamic-ip",         action="store_true",    help="Enable dynamic Ethernet IP addresses setting.")
-    parser.add_target_argument("--eth-reset-time",         default="10e-3",        help="Duration of Ethernet PHY reset.")
-    parser.add_target_argument("--with-hyperram",          action="store_true",    help="Add HyperRAM.")
-    parser.add_target_argument("--with-sdcard",            action="store_true",    help="Add SDCard.")
-    parser.add_target_argument("--with-jtagbone",          action="store_true",    help="Add JTAGBone.")
-    parser.add_target_argument("--with-uartbone",          action="store_true",    help="Add UartBone on 2nd serial.")
-    parser.add_target_argument("--with-video-terminal",    action="store_true",    help="Enable Video Terminal (HDMI).")
-    parser.add_target_argument("--with-video-framebuffer", action="store_true",    help="Enable Video Framebuffer (HDMI).")
-    parser.add_target_argument("--with-spi-flash",         action="store_true",    help="Enable SPI Flash (MMAPed).")
+    from litex.soc.integration.soc import LiteXSoCArgumentParser
+    parser = LiteXSoCArgumentParser(description="LiteX SoC on DDR4 Datacenter Test Board")
+    target_group = parser.add_argument_group(title="Target options")
+    target_group.add_argument("--build",                  action="store_true",    help="Build design")
+    target_group.add_argument("--load",                   action="store_true",    help="Load bitstream")
+    target_group.add_argument("--flash",                  action="store_true",    help="Flash bitstream")
+    target_group.add_argument("--sys-clk-freq",           default=100e6,           help="System clock frequency")
+    target_group.add_argument("--iodelay-clk-freq",       default=200e6,          help="IODELAYCTRL frequency")
+    ethopts = target_group.add_mutually_exclusive_group()
+    ethopts.add_argument("--with-ethernet",         action="store_true",    help="Add Ethernet")
+    ethopts.add_argument("--with-etherbone",        action="store_true",    help="Add EtherBone")
+    target_group.add_argument("--eth-ip",                 default="192.168.1.50", help="Ethernet/Etherbone IP address")
+    target_group.add_argument("--eth-dynamic-ip",         action="store_true",    help="Enable dynamic Ethernet IP addresses setting")
+    target_group.add_argument("--eth-reset-time",         default="10e-3",        help="Duration of Ethernet PHY reset")
+    target_group.add_argument("--with-hyperram",          action="store_true",    help="Add HyperRAM")
+    target_group.add_argument("--with-sdcard",            action="store_true",    help="Add SDCard")
+    target_group.add_argument("--with-jtagbone",          action="store_true",    help="Add JTAGBone")
+    target_group.add_argument("--with-uartbone",          action="store_true",    help="Add UartBone on 2nd serial")
+    target_group.add_argument("--with-video-terminal",    action="store_true",    help="Enable Video Terminal (HDMI)")
+    target_group.add_argument("--with-video-framebuffer", action="store_true",    help="Enable Video Framebuffer (HDMI)")
+    target_group.add_argument("--with-spi-flash",         action="store_true",    help="Enable SPI Flash (MMAPed).")
+    builder_args(parser)
+    soc_core_args(parser)
+    vivado_build_args(parser)
     args = parser.parse_args()
-
-    # Set cpu variant to "minimal" for dram initialization
-    args.update(dict(
-        cpu_variant = "minimal",
-    ))
 
     assert not (args.with_etherbone and args.eth_dynamic_ip)
 
     soc = BaseSoC(
-        sys_clk_freq           = args.sys_clk_freq,
-        iodelay_clk_freq       = args.iodelay_clk_freq,
+        sys_clk_freq           = int(float(args.sys_clk_freq)),
+        iodelay_clk_freq       = int(float(args.iodelay_clk_freq)),
         with_ethernet          = args.with_ethernet,
         with_etherbone         = args.with_etherbone,
         eth_ip                 = args.eth_ip,
@@ -231,10 +219,10 @@ def main():
         with_spi_flash         = args.with_spi_flash,
         with_video_terminal    = args.with_video_terminal,
         with_video_framebuffer = args.with_video_framebuffer,
-        **parser.soc_argdict)
-    builder = Builder(soc, **parser.builder_argdict)
+        **soc_core_argdict(args))
+    builder = Builder(soc, **builder_argdict(args))
     if args.build:
-        builder.build(**parser.toolchain_argdict)
+        builder.build(**vivado_build_argdict(args))
         builder.soc.generate_sdram_phy_py_header(os.path.join(builder.output_dir, "sdram_init.py"))
         # LiteDRAM settings (controller, phy, geom, timing)
         with open(os.path.join(builder.output_dir, 'litedram_settings.json'), 'w') as f:
